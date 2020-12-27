3c4f0621b35228f238ee6a5447b4a20c1ec4d557
Mauro Emmanuel Rambo <mauroemmanuel22@gmail.com>
2020-12-27 02:54:15 -0300

    Logica del ItemDetailContainer

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,8 +1,26 @@
+import {Link} from 'react-router-dom';
+
 export default function ItemDetail(props){
-    const {titulo} = props;
+    const {titulo,bigImg,precio,alt,stock,categoria,descripcion} = props;
+    
+    /*
+    Mejorar los estilos del ItemDetail
+    Se necesita agregar un button para agregar al carrito y 
+    el contador de productos
+    */
+    
     return (
         <>
             <p>{titulo}</p>
+            <img src={bigImg} alt={alt}/>
+            <p>Precio: $<span>{precio}</span></p>
+            <p>Cantidad en Stock: <span>{stock}</span></p>
+            <p>{descripcion}</p>
+            <p>Podés encontrar más productos similares en 
+                <Link to={`/categoria/${categoria}`}>
+                    <span className="category-span">{` ${categoria}`}</span>
+                </Link>
+            </p>
         </>
     )
 }
--- src/components/containers/ItemDetailContainer.jsx
+++ src/components/containers/ItemDetailContainer.jsx
@@ -1,16 +1,58 @@
 import ItemDetail from '../ItemDetail';
 import {useParams} from 'react-router-dom';
-import productos from '../../utils/productos.json';
+import api from '../../utils/productos.json';
+import {useState,useEffect} from 'react';
+import {Bars} from 'svg-loaders-react';
 
 export default function ItemDetailContainer(){
 
+    const [producto,setProducto] = useState();
+    const [isLoading,setLoading] = useState(true);
     const {itemid} = useParams();
-    // eslint-disable-next-line
-    const producto = productos.find(prod=>prod.id==itemid);
 
+
+    const buscarEnApi = new Promise((res,rej)=>{
+        setTimeout(()=>{
+            true?res(api):rej("Error 500");
+                }, 2000)
+        })
+    
+    useEffect(()=>{
+        setLoading(true);
+        buscarEnApi
+        .then(result=>{
+            //eslint-disable-next-line
+            setProducto(api.find(prod=>prod.id==itemid));
+            setLoading(false);
+        })
+        .catch((err)=>{
+            console.error(err);
+        })
+        //eslint-disable-next-line react-hooks/exhaustive-deps
+    },[])
+    
     return(
-        <ItemDetail 
-            titulo={producto.titulo}
-        />
+            <div className="detail-cont">{
+                
+                isLoading?
+                    <Bars fill="brown"/>:   
+                        producto?
+                        <ItemDetail 
+                            titulo={producto.titulo}
+                            bigImg={producto.bigImg}
+                            precio={producto.precio}
+                            alt={producto.alt}
+                            stock={producto.stock}
+                            categoria={producto.categoria}
+                            descripcion={producto.descripcion}
+                        />:
+                        <div>
+                            <h3>Producto no encontrado</h3>
+                            <p>Lo sentimos, el producto que intenta buscar 
+                                no se encuentra o no está disponible.
+                            </p>
+                        </div>
+                }
+            </div>
     )
 }
